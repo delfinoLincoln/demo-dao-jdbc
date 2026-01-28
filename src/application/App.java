0549e80c995848b4f13a316d30f565c2a9cf38eb
thelastvascaino <lincolncdelfino@gmail.com>
2026-01-28 19:00:46 -0300

    up

--- src/application/App.java
+++ src/application/App.java
@@ -1,7 +1,18 @@
 package application;
 
+import models.dao.DaoFactory;
+import models.dao.SellerDao;
+import models.entities.Seller;
+import util.ClearScreen;
+
 public class App {
     public static void main(String[] args) {
-        
+        ClearScreen.cls();
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
+
+        System.out.println(seller);
     }
 }
